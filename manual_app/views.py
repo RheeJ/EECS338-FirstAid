35a9d702cb77abfa41c4070e7a515814312cb7ec
RheeJ <JoonyongRhee2016@u.northwestern.edu>
2016-09-29 19:41:30 -0500

    frontend talking with backend now

--- manual_app/views.py
+++ manual_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.response import Response
 from django.template import loader
+from rest_framework.decorators import api_view
 
+@api_view(['GET'])
 def test_function(request):
-	return HttpResponse("Test Backend Function")
+	return Response("Test Backend Function")
 
--- urls.py
+++ urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = [
 	url(r'^manual_app/', include('manual_app.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('webapp.urls', namespace='webapp')),
 ]
