3d884bab58325ab64ae3b7d6741dfd6b2c70947f
RheeJ <JoonyongRhee2016@u.northwestern.edu>
2016-10-03 21:18:41 -0500

    rest framework setup

---
+++ manual_app/serializers.py
@@ -0,0 +1,19 @@
+from manual_app.models import Graph, Nodes, AdditionalTools
+from rest_framework import serializers
+
+class GraphSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Graph
+        fields = ('name')
+
+class NodesSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Nodes
+		fields = ('step_number', 'instruction', 'graph')
+
+class ATSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = AdditionalTools
+		fields = ('info_name', 'info_type', 'info_path', 'node')
+
+		
--- manual_app/views.py
+++ manual_app/views.py
@@ -4,6 +4,24 @@ from rest_framework.response import Response
 from django.template import loader
 from rest_framework.decorators import api_view
 
+from rest_framework import viewsets
+from rest_framework import permissions
+from manual_app.serializers import GraphSerializer, NodesSerializer, ATSerializer
+
+from manual_app.models import Graph, Nodes, AdditionalTools
+
+class GraphViewSet(viewsets.ModelViewSet):
+	queryset = Graph.objects.all()
+	serializer_class = GraphSerializer
+
+class NodesViewSet(viewsets.ModelViewSet):
+	queryset = Nodes.objects.all()
+	serializer_class = NodesSerializer
+
+class ATViewSet(viewsets.ModelViewSet):
+	queryset = AdditionalTools.objects.all()
+	serializer_class = ATSerializer
+
 @api_view(['GET'])
 def test_function(request):
 	return Response("Test Backend Function")
